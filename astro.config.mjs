d5f51c2abede4508392736cac20d93db2879a72e
TeoryMan <jonathancortestm143@gmail.com>
2023-12-04 02:23:09 -0500

    Fix adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import deno from '@astrojs/deno';
-
 import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: deno(),
+  adapter: vercel(),
 });
